3fab024b9baf04e099a77421367a8e787ad883ee
mustafa laptop <ad16mkha@uwcad.it>
2025-03-11 05:30:05 -0400

    redeploy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,6 @@ export default {
 		extend: {},
 	},
 	plugins: [require("@tailwindcss/typography")],
-	site: 'https://MustafaAnasKH99/.github.io',
-	base: 'my-repo',
+	// site: 'https://MustafaAnasKH99/.github.io',
+	// base: 'my-repo',
 };
